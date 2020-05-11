823a3db92f7e4a75a548ec7c22cd0fead5ab2583
Jun <gene4137@gmail.com>
2020-05-12 00:36:05 +0800

    update switchTheme test

--- tests/unit/switchTheme/switchTheme.steps.ts
+++ tests/unit/switchTheme/switchTheme.steps.ts
@@ -1,12 +1,17 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
+
 import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+
 import Home from '@/views/Home.vue';
 
 const feature = loadFeature('tests/unit/switchTheme/switchTheme.feature');
 
 const localVue = createLocalVue();
+
 localVue.use(Vuex);
+localVue.use(Vuetify);
 
 const colors = {
   namespaced: true,
@@ -64,31 +69,30 @@ const store = new Vuex.Store({
   modules: { colors },
 });
 
-let $vuetify;
+const vuetify = new Vuetify({
+  theme: {
+    dark: false,
+  },
+});
+
 let wrapper: any;
 
 defineFeature(feature, (test) => {
   test('切換系統主題為深色模式', ({ given, when, then }) => {
     given('系統預設主題色為淺色', () => {
-      $vuetify = {
-        theme: {
-          dark: false,
-        },
-      };
-
       wrapper = shallowMount(Home, {
         store,
+        vuetify,
         localVue,
-        mocks: $vuetify,
       });
     });
 
     when('用戶點擊切換主題按鈕', () => {
       wrapper.find('#switchTheme').trigger('click');
+      wrapper.vm.switchTheme();
     });
 
     then('主題色變為深色', () => {
-      wrapper.vm.$vuetify.theme.dark = !wrapper.vm.$vuetify.theme.dark;
       expect(wrapper.vm.$vuetify.theme.dark.valueOf()).toBe(true);
     });
   });
